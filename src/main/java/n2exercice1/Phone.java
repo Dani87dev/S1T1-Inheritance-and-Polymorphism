5bb8f2d2156fb3172771d7f85eab985b4ff4dc82
Daniel Vilà <danivila.dev@gmail.com>
2026-02-08 19:24:14 +0100

    delete unused methods task 2 level 2

--- src/main/java/n2exercice1/Phone.java
+++ src/main/java/n2exercice1/Phone.java
@@ -5,27 +5,13 @@ public class Phone {
 	private String brand;
 	private String model;
 	
-	//Constructor
+
 	public Phone(String marca, String model) {
 		this.brand = marca;
 		this.model = model;
 	}
 	
-	//Getters && Setters
-	public String getMarca() {
-		return brand;
-	}
-	public String getModel() {
-		return model;
-	}
-	public void setMarca(String marca) {
-		this.brand = marca;
-	}
-	public void setModel(String model) {
-		this.model = model;
-	}
-	
-	//Métodes
+
 	public void call(String numTel) {
 		System.out.println("The number " + numTel + " is being called");
 	}
